b9792450d7ad6abce6f9f7f319a33841bc4ed7c9
soyaka <anassbenhalima@gmail.com>
2024-10-17 16:06:08 +0100

    setup

--- api/obj/Debug/net8.0/..AssemblyInfo.cs
+++ api/obj/Debug/net8.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2483b9382a41694d2e791e6b0ae23803fbcaa743")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
